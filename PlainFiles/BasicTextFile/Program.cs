6225dbd0cdec3bf2d9cc9f88f723f2c05d45f849
Juan Zuluaga <jzuluaga@planatechnologies.com>
2025-05-22 19:46:05 -0500

    Basic text file

---
+++ PlainFiles/BasicTextFile/Program.cs
@@ -0,0 +1,75 @@
+﻿using BasicTextFile;
+
+var textFile = new SimpleTextFile("data.txt");
+var lines = textFile.ReadLines();
+var opc = "0";
+
+do
+{
+    opc = Menu();
+    Console.WriteLine("=======================================");
+    switch (opc)
+    {
+        case "1":
+            if (lines.Length == 0)
+            {
+                Console.WriteLine("Empty file.");
+                break;
+            }
+            foreach (var line in lines)
+            {
+                Console.WriteLine(line);
+            }
+            break;
+
+        case "2":
+            Console.Write("Enter the line to add: ");
+            var newLine = Console.ReadLine();
+            if (!string.IsNullOrEmpty(newLine))
+            {
+                lines = lines.Append(newLine).ToArray();
+            }
+            break;
+
+        case "3":
+            Console.Write("Enter the line to remove: ");
+            var lineToRemove = Console.ReadLine();
+            if (!string.IsNullOrEmpty(lineToRemove))
+            {
+                lines = lines.Where(line => line != lineToRemove).ToArray();
+            }
+            break;
+
+        case "4":
+            SaveChanges();
+            break;
+
+        case "0":
+            Console.WriteLine("Exiting...");
+            break;
+
+        default:
+            Console.WriteLine("Invalid option. Please try again.");
+            break;
+    }
+} while (opc != "0");
+SaveChanges();
+
+void SaveChanges()
+{
+    Console.WriteLine("Saving changes...");
+    textFile.WriteLines(lines);
+    Console.WriteLine("Changes saved.");
+}
+
+string Menu()
+{
+    Console.WriteLine("=======================================");
+    Console.WriteLine("1. Show content");
+    Console.WriteLine("2. Add line");
+    Console.WriteLine("3. Remove line");
+    Console.WriteLine("4. Save changes");
+    Console.WriteLine("0. Exit");
+    Console.Write("Enter your option: ");
+    return Console.ReadLine() ?? "0";
+}
---
+++ PlainFiles/BasicTextFile/SimpleTextFile.cs
@@ -0,0 +1,26 @@
+﻿namespace BasicTextFile;
+
+public class SimpleTextFile
+{
+    private string _path;
+
+    public SimpleTextFile(string path)
+    {
+        _path = path;
+    }
+
+    public void WriteLines(string[] lines)
+    {
+        File.WriteAllLines(_path, lines);
+    }
+
+    public string[] ReadLines()
+    {
+        if (!File.Exists(_path))
+        {
+            return [];
+        }
+
+        return File.ReadAllLines(_path);
+    }
+}
