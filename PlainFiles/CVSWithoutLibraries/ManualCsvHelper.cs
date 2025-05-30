73360229c1652b682063350583f55b30ba120a73
Juan Zuluaga <jzuluaga@planatechnologies.com>
2025-05-29 19:11:49 -0500

    CVS Without library

---
+++ PlainFiles/CVSWithoutLibraries/ManualCsvHelper.cs
@@ -0,0 +1,31 @@
+﻿namespace CVSWithoutLibraries;
+
+public class ManualCsvHelper
+{
+    public void WriteCVS(string path, List<string[]> records)
+    {
+        using var sw = new StreamWriter(path);
+        foreach (var record in records)
+        {
+            var line = string.Join(",", record);
+            sw.WriteLine(line);
+        }
+    }
+
+    public List<string[]> ReadCVS(string path)
+    {
+        var records = new List<string[]>();
+        if (!File.Exists(path))
+        {
+            return records;
+        }
+        using var sr = new StreamReader(path);
+        string? line;
+        while ((line = sr.ReadLine()) != null)
+        {
+            var record = line.Split(',');
+            records.Add(record);
+        }
+        return records;
+    }
+}
---
+++ PlainFiles/CVSWithoutLibraries/Program.cs
@@ -0,0 +1,19 @@
+﻿using CVSWithoutLibraries;
+
+var people = new List<string[]>
+{
+    new[] { "Id", "Name", "Age" },
+    new[] { "1", "Alice", "30" },
+    new[] { "2", "Bob", "25" },
+    new[] { "3", "Pedro", "45" },
+    new[] { "4", "Maria", "35" },
+    new[] { "5", "Edison", "18" },
+};
+
+var manualCVS = new ManualCsvHelper();
+manualCVS.WriteCVS("people.csv", people);
+var readPeople = manualCVS.ReadCVS("people.csv");
+foreach (var person in readPeople)
+{
+    Console.WriteLine(string.Join(", ", person));
+}
