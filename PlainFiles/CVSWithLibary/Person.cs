71b5fc0ae7303ed5c936a44a86ec15559189306a
Juan Zuluaga <jzuluaga@planatechnologies.com>
2025-05-29 20:08:38 -0500

    Add menu

--- PlainFiles/CVSWithLibary/Person.cs
+++ PlainFiles/CVSWithLibary/Person.cs
@@ -3,6 +3,17 @@
 public class Person
 {
     public int Id { get; set; }
-    public string Name { get; set; } = null!;
-    public int Age { get; set; }
+    public string FirstName { get; set; } = null!;
+    public string LastName { get; set; } = null!;
+    public string Phone { get; set; } = null!;
+    public string City { get; set; } = null!;
+    public decimal Balance { get; set; }
+
+    public override string ToString()
+    {
+        return $"{Id}\t{FirstName} {LastName}\n\t" +
+            $"Phone: {Phone}\n\t" +
+            $"City: {City}\n\t" +
+            $"Balance: {Balance,20:C2}\n";
+    }
 }
--- PlainFiles/CVSWithLibary/Program.cs
+++ PlainFiles/CVSWithLibary/Program.cs
@@ -1,15 +1,67 @@
 ﻿using CVSWithLibary;
 
-var list = new List<Person>
-{
-    new() { Id = 1, Name = "Maria", Age = 28 },
-    new() { Id = 2, Name = "Juan", Age = 34 }
-};
 var helper = new CsvHelperExample();
-helper.Write("people.csv", list);
-
-var readPeople = helper.Read("people.csv");
-foreach (var person in readPeople)
+var readPeople = helper.Read("people.csv").ToList();
+var opc = "0";
+do
 {
-    Console.WriteLine($"Id: {person.Id}, Name: {person.Name}, Age: {person.Age}");
+    opc = Menu();
+    Console.WriteLine("=======================================");
+    switch (opc)
+    {
+        case "1":
+            foreach (var person in readPeople)
+            {
+                Console.WriteLine(person);
+            }
+            break;
+
+        case "2":
+            Console.Write("Enter the ID: ");
+            var id = Console.ReadLine();
+            Console.Write("Enter the First name: ");
+            var firstName = Console.ReadLine();
+            Console.Write("Enter the Last name: ");
+            var lastName = Console.ReadLine();
+            Console.Write("Enter the phone: ");
+            var phone = Console.ReadLine();
+            Console.Write("Enter the city: ");
+            var city = Console.ReadLine();
+            Console.Write("Enter the balance: ");
+            var balance = Console.ReadLine();
+
+            var newPerson = new Person
+            {
+                Id = int.Parse(id!),
+                FirstName = firstName ?? string.Empty,
+                LastName = lastName ?? string.Empty,
+                Phone = phone ?? string.Empty,
+                City = city ?? string.Empty,
+                Balance = decimal.Parse(balance!)
+            };
+
+            readPeople.Add(newPerson);
+            break;
+
+        case "3":
+            SaveChanges();
+            break;
+    }
+} while (opc != "0");
+SaveChanges();
+
+void SaveChanges()
+{
+    helper.Write("people.csv", readPeople);
+}
+
+string Menu()
+{
+    Console.WriteLine("=======================================");
+    Console.WriteLine("1. Show content");
+    Console.WriteLine("2. Add person");
+    Console.WriteLine("3. Save changes");
+    Console.WriteLine("0. Exit");
+    Console.Write("Choose an option: ");
+    return Console.ReadLine() ?? "0";
 }
